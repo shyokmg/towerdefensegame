13254bb55dcf9c0d4330334db40ba5b22f131c79
shyokmg <139237729+shyokmg@users.noreply.github.com>
2026-06-07 14:34:38 +1000

    commit-07-06-26
    - added path simple finding to enemy
    - changed assets to simple shape

---
+++ scenes/enemy/enemy.gd
@@ -0,0 +1,18 @@
+extends CharacterBody2D
+
+
+const speed = 50
+@export var playerBase: Node2D
+@onready var nav_agent := $NavigationAgent2D as NavigationAgent2D
+
+func _physics_process(delta: float) -> void:
+	var dir = to_local(nav_agent.get_next_path_position()).normalized()
+	velocity = dir * speed
+	move_and_slide()
+	
+func makepath() -> void:
+	nav_agent.target_position = playerBase.global_position
+	
+
+func _on_timer_timeout() -> void:
+	makepath()
